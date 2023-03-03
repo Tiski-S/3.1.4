a512eecbb59dbbf73285786312b7caef8f35d430
Andrei Kryvaltsevich <krays170@gmail.com>
2023-03-03 14:54:21 +0300

    Second

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -69,6 +69,7 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public void updateUser(User user) {
+        user.setA(user.getPassword());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
